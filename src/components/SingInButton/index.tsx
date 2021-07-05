2331500a91266bfaef98911ee70bf884a8f5ddd2
DevClaudioFilho <devclaudiofilho@gmail.com>
2021-07-05 21:34:25 +0100

    implement a faunadb and nextAuth with github

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -2,17 +2,21 @@ import styles from "./styles.module.scss";
 import {FaGithub} from "react-icons/fa";
 import {FiX} from "react-icons/fi";
 
-export function SingInButton(){
-    const isUserLoggedIn = true;
 
-    return isUserLoggedIn ?
+import { signIn,signOut,useSession } from "next-auth/client";
+
+export function SingInButton(){
+    const [session] = useSession();
+
+    return session ?
     (
         <button
         type='button'
-        className={styles.singInButton}   
+        className={styles.singInButton}  
+        onClick={()=>signOut()}
         >
             <FaGithub color="#04d361"/>
-            Claudio Filho
+            {session.user.name}
             <FiX color="#737380 " className={styles.closeIcon}/>
         </button>
     ):
@@ -20,6 +24,7 @@ export function SingInButton(){
         <button
         type='button'
         className={styles.singInButton}   
+        onClick={()=>signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sing in with Github
