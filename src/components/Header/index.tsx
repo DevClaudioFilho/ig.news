66785d03dea1dadcecfc4cdef2fd0f030920f432
DevClaudioFilho <devclaudiofilho@gmail.com>
2021-07-23 20:02:04 +0100

    increment a post page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
+
 import { SingInButton } from "../SingInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from '../ActiveLink';
 
 export function Header(){
+
     return(
         <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
             <img src="/images/logo.svg" alt="ig.news" />
             <nav>
-                <a className={styles.active} href="">Home</a>
-                <a href="">Posts</a>
+                <ActiveLink href="/" activeClassName={styles.active} >
+                    <a  href="">Home</a>
+                </ActiveLink>
+                <ActiveLink  href="/posts"  activeClassName={styles.active} >
+                    <a  href="">Posts</a>
+                </ActiveLink>
+               
+               
             </nav>
 
             <SingInButton/>
